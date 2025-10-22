dcfccba4d78a89a112ce14e1eb1ea932a1bb492b
Stiaan <stiaant1@gmail.com>
2025-10-22 09:38:52 +0200

    feat: initialize CDK project with TypeScript setup
    - Add package.json with dependencies and scripts for building, testing, and deploying the CDK project.
    - Create initial test file for SQS Queue resource with example test case.
    - Add tsconfig.json for TypeScript configuration with strict type checking and ES2022 target.

---
+++ test/week-10-cdk-project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Week10CdkProject from '../lib/week-10-cdk-project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/week-10-cdk-project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Week10CdkProject.Week10CdkProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
